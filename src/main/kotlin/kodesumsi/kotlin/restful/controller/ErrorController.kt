845ff0934e551bbb7fb1388ffe1d7af54df04a0d
Hilman Taris Muttaqin <hilmanmuttaqin345@gmail.com>
2020-10-15 15:50:11 +0700

    Implement validation exception handler

---
+++ src/main/kotlin/kodesumsi/kotlin/restful/controller/ErrorController.kt
@@ -0,0 +1,22 @@
+package kodesumsi.kotlin.restful.controller
+
+import kodesumsi.kotlin.restful.model.WebResponse
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.RestControllerAdvice
+import javax.validation.ConstraintViolationException
+
+@RestControllerAdvice
+class ErrorController {
+
+    @ExceptionHandler(value = [ConstraintViolationException::class])
+    fun validationHandler(
+            constraintViolationException: ConstraintViolationException
+    ): WebResponse<String> {
+        return WebResponse(
+                code = 400,
+                status = "BAD REQUEST",
+                data = constraintViolationException.message!!
+        )
+    }
+
+}
