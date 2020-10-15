485633afcd36908e0d94c256f4f3b232b528f60e
Hilman Taris Muttaqin <hilmanmuttaqin345@gmail.com>
2020-10-15 14:50:02 +0700

    Implement create product controller

--- src/main/kotlin/kodesumsi/kotlin/restful/controller/ProductController.kt
+++ src/main/kotlin/kodesumsi/kotlin/restful/controller/ProductController.kt
@@ -1,7 +1,33 @@
 package kodesumsi.kotlin.restful.controller
 
+import kodesumsi.kotlin.restful.model.CreateProductRequest
+import kodesumsi.kotlin.restful.model.ProductResponse
+import kodesumsi.kotlin.restful.model.WebResponse
+import kodesumsi.kotlin.restful.service.ProductService
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class ProductController {
+class ProductController(
+        val productService: ProductService
+) {
+
+    @PostMapping(
+            value = ["/api/products"],
+            produces = ["aplication/json"],
+            consumes = ["aplication/json"]
+    )
+    fun createProduct(
+            @RequestBody createProductRequest: CreateProductRequest
+    ): WebResponse<ProductResponse> {
+        val productResponse = productService.create(createProductRequest)
+
+        return WebResponse(
+                code = 200,
+                status = "OK",
+                data = productResponse
+        )
+    }
+
 }
